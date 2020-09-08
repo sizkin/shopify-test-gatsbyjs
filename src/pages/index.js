ff3dc61b7a509988dcbc083553d29f5298fb7aa1
Nick Chan <nick.chan@aitherentertainment.com>
2020-09-08 13:55:22 +0800

    Init shopify test

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,48 @@
 import React from "react"
-import { Link } from "gatsby"
+import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <h1>List Products from shopify</h1>
+    <div>
+      {data.allProducts.edges.map(({ node }) => (
+        <div key={node.shopifyId}>
+          <h3>{node.title}</h3>
+          <p>{node.description}</p>
+          <span>{node.priceRange.minVariantPrice.currencyCode} {node.priceRange.minVariantPrice.amount}</span>
+        </div>
+      ))}
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
 )
 
 export default IndexPage
+
+export const query = graphql`
+{
+  allProducts: allShopifyProduct(sort: { fields: [title] }) {
+    edges {
+      node {
+        shopifyId
+        title
+        description
+        handle
+        priceRange {
+          maxVariantPrice {
+            amount
+            currencyCode
+          }
+          minVariantPrice {
+            amount
+            currencyCode
+          }
+        }
+      }
+    }
+  }
+}
+`
